9f6cf1b6b7d7f6327b78039af13431cbb63ec68f
Junior SANNI <80217654+Junior12345-cmd@users.noreply.github.com>
2025-04-13 01:41:45 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.title("TP2 : Clustering")
 col1, col2 = st.columns(2)
 with col1:
     if st.button("K-means Clustering 🟢"):
-        st.switch_page("pages/1_Kmeans.py")
+        st.switch_page("pages/kmeans.py")
 with col2:
     if st.button("Hierarchical Clustering 🔵"):
-        st.switch_page("pages/2_Hclust.py")
+        st.switch_page("pages/hclust.py")
