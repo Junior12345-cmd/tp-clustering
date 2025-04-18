98b4dc39c85bf711428e446ea9384b4c23e42287
Junior SANNI <80217654+Junior12345-cmd@users.noreply.github.com>
2025-04-18 12:50:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("TP2 : Clustering K-means/Hclust")
+    st.title("TP2 : Clustering K-means-=Hclust")
     st.write("Bienvenue sur le TP de Junior SANNI | MASTER 1 | 2024-2025. Utilisez le menu de gauche pour accéder aux différentes pages.")
     
     # Pour vérifier que tout fonctionne
